9c211fe3a398f164f24585ca9e4013da2370297c
DevanshuRastogi <125262981+DevanshuRastogi@users.noreply.github.com>
2025-03-09 18:51:52 +0530

    task api 02

--- 02TaskManagerAPI/app.js
+++ 02TaskManagerAPI/app.js
@@ -3,5 +3,5 @@ const app = express();
 const PORT = 3000
 const taskRouters = require("./routes/taskRouter")
 
-app.use("/" ,taskRouters );
+app.use("/api/v1/tasks" ,taskRouters );
 app.listen(3000 , console.log(`Server is up and running at ${PORT}`))
