96ebca66d5c533f19f2960aa5e62e182cd60b3f7
DevanshuRastogi <125262981+DevanshuRastogi@users.noreply.github.com>
2025-03-15 22:29:15 +0530

    pagination using skip and limit

--- 03StoreAPI/controllers/products.js
+++ 03StoreAPI/controllers/products.js
@@ -1,7 +1,7 @@
 const products = require("../models/product");
 
 const productRoute = async (req, res) => {
-  let { featured, company,name,sort , select } = req.query;
+  let { featured, company, name, sort, select, page, limit } = req.query;
   const queryObject = new Object();
   if (featured) {
     queryObject.featured = featured === true ? true : false;
@@ -9,24 +9,29 @@ const productRoute = async (req, res) => {
   if (company) {
     queryObject.company = company;
   }
-if(name){
-     queryObject.name = { $regex:name , $options :'i' }
-}
-let result =  products.find(queryObject);
-if(sort){
-     const sortList = sort.split(",").join(" ")
-      console.log(sortList)
-     result=result.sort(sortList);
-}
-else{
-     result=result.sort("createdAt");
-}
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  let result = products.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    console.log(sortList);
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
 
-if(select){
-     select= select.split(",").join(" ")
-     result = result.select(select);
-}
-const prod = await result;
+  if (select) {
+    select = select.split(",").join(" ");
+    result = result.select(select);
+  }
+
+  page = Number(page)||1;
+  limit = Number(limit)||10;
+let skip = (page-1)*limit
+
+result = result.skip(skip).limit(limit);
+  const prod = await result;
   console.log(queryObject);
   res.status(200).json({ nBhits: prod.length, prod });
 };
