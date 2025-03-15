4bbb158c0ae699deb76b683e822e3651ab9e22e8
DevanshuRastogi <125262981+DevanshuRastogi@users.noreply.github.com>
2025-03-15 11:42:27 +0530

    query object done

--- 03StoreAPI/controllers/products.js
+++ 03StoreAPI/controllers/products.js
@@ -1,9 +1,18 @@
-const productRoute = (req,res) =>{
-     res.status(200).json({message:`Product page`})
-}
-const productStaticRoute = (req,res) =>{
-    throw new Error('async-express-error trying')
-     res.status(200).json({message:`Product STatic page`})
-}
+const products = require("../models/product");
 
-module.exports = {productRoute , productStaticRoute};
+const productRoute = async (req, res) => {
+  const { featured } = req.query;
+  const queryObject = new Object();
+  if (featured) {
+    queryObject.featured = featured === true ? true : false;
+  }
+  prod = await products.find(queryObject);
+  console.log(queryObject);
+  res.status(200).json({  nBhits: prod.length, prod });
+};
+const productStaticRoute = (req, res) => {
+  //     throw new Error('async-express-error trying')
+  res.status(200).json({ message: `Product STatic page` });
+};
+
+module.exports = { productRoute, productStaticRoute };
